ee4b353d91895f0fdeeecef172548b5f7f63bfea
Ahsan <ahsanan@outlook.com>
2024-04-27 16:05:25 +0600

    migration done

---
+++ database/migrations/2024_04_27_100335_create_leave_requests_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeaveRequestsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('leave_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->enum('leave_type', ['Casual Leave', 'Sick Leave', 'Emergency Leave']);
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->text('reason');
+            $table->enum('status', ['Pending', 'Approved', 'Rejected'])->default('Pending');
+            $table->text('admin_comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('leave_requests');
+    }
+}
